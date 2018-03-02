a3bad22470158e5ef06d07780fdd2996ab97fa0d
Nix <yiunixon@gmail.com>
2018-03-01 20:40:37 -0800

    add public column to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302043252) do
+ActiveRecord::Schema.define(version: 20180302043805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180302043252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "hot", default: false
+    t.boolean "public", default: true
     t.index ["drink_id"], name: "index_reviews_on_drink_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
