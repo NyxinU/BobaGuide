c6ab0816fcb7376b170833db2c576c0afd03801b
Betty <b.lam4392@gmail.com>
2018-03-04 22:06:39 -0800

    Annotate Review  model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: menus
+#
+#  id         :integer          not null, primary key
+#  store_id   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Menu < ApplicationRecord
   validates :store_id, presence: true, uniqueness: true
 
--- test/models/menu_test.rb
+++ test/models/menu_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: menus
+#
+#  id         :integer          not null, primary key
+#  store_id   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class MenuTest < ActiveSupport::TestCase
