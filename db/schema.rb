15aaeef64e390065912a0ee3d918fd23bb0a273e
Nix <yiunixon@gmail.com>
2018-03-30 16:57:53 -0700

    change menu store id to int

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330233233) do
+ActiveRecord::Schema.define(version: 20180330234327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,9 @@ ActiveRecord::Schema.define(version: 20180330233233) do
   end
 
   create_table "menus", force: :cascade do |t|
-    t.string "store_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["store_id"], name: "index_menus_on_store_id"
+    t.integer "store_id", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
