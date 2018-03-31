94e6ca308120364fd26edcb57e363e1c02a7fa51
Nix <yiunixon@gmail.com>
2018-03-31 15:02:15 -0700

    populate store information

--- frontend/components/store/store_information.jsx
+++ frontend/components/store/store_information.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const StoreInformation = ({ store }) => {
-  // console.log(store)
+  console.log(store)
   return (
     <div>
-      
+      <h1>{store.name}</h1>
+      <div>{store.rating}</div>
+      <span>{store.review_count}</span>
+      <span>{store.price}</span>
+      <div>replace this with a static map</div>
+      <address>
+        {`${store.location.display_address[0]}`}
+        <br />
+        {`${store.location.display_address[1]}`}
+      </address>
     </div>
   );
 };
--- frontend/components/store/store_show.jsx
+++ frontend/components/store/store_show.jsx
@@ -12,14 +12,9 @@ class StoreShow extends React.Component {
     if (prevProps.match.params.businessId !== this.props.match.params.businessId) {
       this.props.fetchStore(this.props.match.params.businessId);
     }
-    if (this.props.match.params.businessId !== Object.keys(this.props.store)[0]) {
-      console.log(this.props.match.params.businessId);
-      console.log(Object.keys(this.props.store)[0]);
-    }
   }
 
   render() {
-    console.log(this.props);
     if (this.props.store === undefined) {
       return (
         <div />
