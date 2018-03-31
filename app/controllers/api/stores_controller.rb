c451ca9c2fdd834d3e7f3a32f0c9e08dbbf887ef
Nix <yiunixon@gmail.com>
2018-03-30 19:15:13 -0700

    hide api key

--- app/controllers/api/stores_controller.rb
+++ app/controllers/api/stores_controller.rb
@@ -5,7 +5,7 @@ class Api::StoresController < ApplicationController
 
 
   # Constants, do not change these
-  API_KEY = '467mR1Rp6HH3uwrJdQulLhSstM1SMXY_LJyCKZD_DgVuYPUTVRh9YFbXES9vrUOiVmX32U04PYyMK7c38tzd0JNaIyny_pdVf0dnrJNTI6ak6cpoCwkOl2QlJVqOWnYx'
+  API_KEY = ''
   API_HOST = "https://api.yelp.com"
   SEARCH_PATH = "/v3/businesses/search"
   BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
